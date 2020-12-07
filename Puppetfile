a3e5c2c9ecf8842f38f786ad26110377894e4b2b
Chris Southall <southallc@gmail.com>
2020-12-06 21:40:36 -0600

    update hiera data files

--- Puppetfile
+++ Puppetfile
@@ -1,9 +1,26 @@
 # https://puppet.com/docs/pe/latest/puppetfile.html
 forge 'forge.puppetlabs.com'
+
+# Supported modules
 mod 'puppetlabs/stdlib', :latest
 mod 'puppetlabs/concat', :latest
-mod 'puppet/archive', :latest
 mod 'puppetlabs/inifile', :latest
+mod 'puppetlabs/apt', :latest
+mod 'puppetlabs-powershell', :latest
+mod 'puppetlabs-pwshlib', :latest
+mod 'puppetlabs-registry', :latest
+mod 'puppetlabs-acl', :latest
+
+# Modules removed from Puppet 6.x
 mod 'puppetlabs/k5login_core', :latest
+mod 'puppetlabs-mailalias_core', :latest
+
+# Approved modules
+mod 'puppet/archive', :latest
 mod 'puppet/firewalld', :latest
+mod 'puppet-windows_env', :latest
+
+# My own modules
+mod 'southalc/podman', :latest
 mod 'southalc/types', :latest
+
--- manifests/site.pp
+++ manifests/site.pp
@@ -1,20 +1,22 @@
-# Lookup classes to include from hiera
-lookup('classes', Array[String], 'unique').include
+# Include classes from hiera lookup, enabling specific exclusions
+$hiera_classes          = lookup('classes',          Array[String], 'unique', [])
+$hiera_class_exclusions = lookup('class_exclusions', Array[String], 'unique', [])
+include  [ $hiera_classes - $hiera_class_exclusions ]
 
-
-# Example for assigning variables to nodes by node name or other rules
-# - node-scope variables must be set BEFORE the hiera lookup for classes, so comment out the above lookup to use rules like these examples
-# - When managing many nodes, it's more convenient to use an [ENC](https://github.com/southalc/enc)
+# Example for node classification with node scope variables from the manifest
+# - Set node-scope variables BEFORE a hiera lookup for classes
+# - I find it more convenient to use an [ENC](https://github.com/southalc/enc)
 ########################################################################
 # if $::trusted['certname'] == 'jenkins.example.com' {
 #   $_role = 'jenkins'
 # }
-# # Set the 'win10_dev' role for all Windows 10 machines determined by facts and hostname
+# Set the 'win10_dev' role for all Windows 10 machines determined by facts and hostname
+# - You could also do this by referencing facts in hiera.yaml
 # else (($::facts['os']['family'] == 'windows') and ($::facts['os']['release']['major'] == '10')) and ($::hostname =~ /^dev(.+)/) {
 #   $_role = 'win10_dev'
 # }
 # 
-# # Use '$_role' as a node-scope variable and include classes from hiera
+# Use '$_role' as a node-scope variable and include classes from hiera
 # node default {
 #   $role = $_role
 #   lookup('classes', Array[String], 'unique').include
